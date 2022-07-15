95e385138826b2e57dc067f9b2728ee24143eb82
Doug Kulak <doug.kulak@gmail.com>
2022-07-14 21:51:29 -0700

    fix: Button defined but never used

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,4 +1,4 @@
-import {Box, Button, HStack, Text} from '@chakra-ui/react';
+import {Box, HStack, Text} from '@chakra-ui/react';
 import {useRecoilValue} from 'recoil';
 import {playerState} from '../state/playerState';
 
